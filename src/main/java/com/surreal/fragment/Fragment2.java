6061da45dac258c777a8e6a7d3e2f9c86c479d0c
Abhay-Code-Zone <abhay@example.com>
2023-12-12 20:59:00 +0530

    feat: It spoke of forgotten colors

---
+++ src/main/java/com/surreal/fragment/Fragment2.java
@@ -0,0 +1,13 @@
+package com.surreal.fragment;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Fragment2 {
+    
+    private final String narrative = "It spoke of forgotten colors";
+    
+    public String getNarrative() {
+        return narrative;
+    }
+}
